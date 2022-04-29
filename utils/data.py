59663a62da474f8da5a60ec48b884d6941c68775
ankile <lars.ankile@gmail.com>
2022-04-29 14:15:44 +0200

    Fix datetime format

--- utils/data.py
+++ utils/data.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 # Defined globally where the test set starts in the complete dataset
-test_start_str = "01.01.2019"
+test_start_str = "2019-01-01"
 test_start_pd = pd.to_datetime(test_start_str)
