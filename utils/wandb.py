890a44bed35d15b3171f66b57cbab6cc3d3890cc
ankile <lars.ankile@gmail.com>
2022-04-29 11:31:32 +0200

    Add put dataset function to wandb utils

--- utils/wandb.py
+++ utils/wandb.py
@@ -2,10 +2,20 @@ import wandb as wb
 import pandas as pd
 
 
-def get_df_artifact(name):
-    with wb.init(project="master-test") as run:
+def get_df_artifact(name: str, project: str):
+    with wb.init(project=project) as run:
         art = run.use_artifact(name)
         art.download()
         filepath = art.file()
 
         return pd.read_feather(filepath)
+
+
+def put_dataset(df: pd.DataFrame, filename: str, project: str):
+    df.reset_index().to_feather(filename)
+
+    artifact = wb.Artifact(filename.split(".")[0], type="dataset")
+    artifact.add_file(filename)
+
+    with wb.init(project=project) as run:
+        run.log_artifact(artifact)
