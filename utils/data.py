75d14640bde2723e293bf20f605729fc48d17943
ankile <lars.ankile@gmail.com>
2022-05-04 14:04:01 +0200

    Fix get test set function

--- utils/data.py
+++ utils/data.py
@@ -6,14 +6,12 @@ test_start_str = "2019-01-01"
 test_start_pd = pd.to_datetime(test_start_str)
 
 
-obsnum = lambda df: list(range(df.shape[0], 0, -1))
+obsnum = lambda df, asc: sorted(range(1, df.shape[0]+1), reverse=not asc)
 
 
 def get_1_month_test_set(df):
-    out = df[df.index >= test_start_pd]
-    out.loc[:, "obs_number"] = (
-        out.groupby("ticker").date.rank(method="first", ascending=True).astype(int)
-    )
+    out = df[df.index >= test_start_pd].copy()
+    out.loc[:, "obs_number"] = obsnum(out, asc=True)
     return out[out.obs_number <= 20].drop(columns=["obs_number"])
 
 
@@ -21,7 +19,7 @@ def get_train_set(df, limit=None):
     out = df[df.index < test_start_pd].copy()
 
     if limit is not None:
-        out.loc[:, "obs_number"] = obsnum(out)
+        out.loc[:, "obs_number"] = obsnum(out, asc=False)
         out = out[out.obs_number <= limit].drop(columns=["obs_number"])
 
     return out
