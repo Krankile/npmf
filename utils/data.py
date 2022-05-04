f026a78a377e13caa4b79302760180c36af35750
ankile <lars.ankile@gmail.com>
2022-05-04 13:39:29 +0200

    Add obsnum function

--- utils/data.py
+++ utils/data.py
@@ -6,9 +6,12 @@ test_start_str = "2019-01-01"
 test_start_pd = pd.to_datetime(test_start_str)
 
 
+obsnum = lambda df: list(range(df.shape[0], 0, -1))
+
+
 def get_1_month_test_set(df):
     out = df[df.index >= test_start_pd]
-    out["obs_number"] = (
+    out.loc[:, "obs_number"] = (
         out.groupby("ticker").date.rank(method="first", ascending=True).astype(int)
     )
     return out[out.obs_number <= 20].drop(columns=["obs_number"])
@@ -18,7 +21,10 @@ def get_train_set(df, limit=None):
     out = df[df.index < test_start_pd]
 
     if limit is not None:
-        out["obs_number"] = out.groupby("ticker").date.rank(method='first', ascending=False).astype(int)
-        out = out[out.obs_number <= limit].drop(columns=["obs_number"])
+        out.loc[:, "obs_number"] = (
+            out.groupby("ticker").date.rank(method="first", ascending=False).astype(int)
+        )
+        out = out[out.obs_number <= limit]
+        out = out.drop(columns=["obs_number"])
 
     return out
