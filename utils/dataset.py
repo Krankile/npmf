42bd88cd3b208502e674f06447be4812874b5d66
ankile <lars.ankile@gmail.com>
2022-05-20 11:44:58 +0200

    Add date range getters

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import minmax_scale
@@ -20,3 +22,20 @@ def get_stocks_in_timeframe(stock_df, stock_dates, scale=True):
             columns=out.columns,
         )
     return out
+
+
+def get_historic_dates(current_time, trading_days):
+    back_in_time_buffer = timedelta(trading_days + trading_days * 5)
+
+    return pd.date_range(
+        start=current_time - back_in_time_buffer, end=current_time, freq="B"
+    )[-trading_days:]
+
+
+def get_forecast_dates(current_time, forecast_window):
+    forward_in_time_buffer = timedelta(forecast_window + forecast_window * 5)
+    return pd.date_range(
+        start=current_time + timedelta(1),
+        end=current_time + forward_in_time_buffer,
+        freq="B",
+    )[:forecast_window]
