f9111c1546a51b7ad0b311664cc526d31cd7f236
Kjartan Krange <kjartankrange@outlook.com>
2022-05-26 19:25:29 +0200

    Fix denominator of na_percentage

--- utils/dataset.py
+++ utils/dataset.py
@@ -270,7 +270,7 @@ def get_target(
     return targets_normalized
 
 def register_na_percentage(dictionary: dict, df_nick_name: str, df: pd.DataFrame):
-    dictionary[df_nick_name] = df.isnull().sum().sum()/df.shape[0]
+    dictionary[df_nick_name] = df.isnull().sum().sum()/(df.shape[0]*df.shape[1])
 
 class TimeDeltaDataset(Dataset):
     def __init__(
