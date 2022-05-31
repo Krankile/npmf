ff4ce9a45a0c2f05e612f0ff0ef53e045b85dc25
ankile <lars.ankile@gmail.com>
2022-05-31 12:36:34 +0200

    Refactor models in utils

---
+++ utils/models/__init__.py
@@ -0,0 +1,9 @@
+from .naive_model import naive_models
+from .tcn_model import tcn_models
+
+
+models = {**naive_models, **tcn_models}
+
+__all__ = [
+    models
+]
--- utils/models/tcn_model.py
+++ utils/models/tcn_model.py
@@ -5,81 +5,6 @@ from torch import nn
 from torch.nn.utils import weight_norm
 
 
-class NaiveMultivariateNetworkV1(nn.Module):
-    def __init__(
-        self,
-        sf_len_in,
-        sf_len_out,
-        meta_cont_lens,
-        meta_cat_lens,
-        meta_hd,
-        macro_len_in,
-        macro_len_out,
-        hd1,
-        hd2,
-        out_len,
-        d1,
-        d2,
-        d3,
-        **_,
-    ):
-        super().__init__()
-
-        self.sf = nn.Sequential(
-            nn.Linear(sf_len_in, sf_len_out),
-            nn.ReLU(),
-        )
-
-        self.meta_cont = nn.Sequential(
-            nn.Linear(meta_cont_lens[0], meta_cont_lens[1]),
-            nn.ReLU(),
-        )
-
-        self.meta_cat = nn.ModuleList(
-            [nn.Embedding(in_, out) for in_, out in meta_cat_lens]
-        )
-
-        self.meta_hidden = nn.Sequential(
-            nn.Linear(
-                meta_cont_lens[1] + sum(map(itemgetter(1), meta_cat_lens)), meta_hd
-            ),
-            nn.ReLU(),
-        )
-
-        self.macro = nn.Sequential(
-            nn.Linear(macro_len_in, macro_len_out),
-            nn.ReLU(),
-        )
-
-        total_width = sum([sf_len_out, macro_len_out, meta_hd])
-
-        self.predict = nn.Sequential(
-            nn.Dropout(p=d1),
-            nn.Linear(total_width, hd1),
-            nn.ReLU(),
-            nn.Dropout(p=d2),
-            nn.Linear(hd1, hd2),
-            nn.ReLU(),
-            nn.Dropout(p=d3),
-            nn.Linear(hd2, out_len),
-        )
-
-    def forward(self, sf, meta_cont, meta_cat, macro):
-
-        sf = self.sf(sf)
-        meta = torch.cat(
-            [self.meta_cont(meta_cont)]
-            + [emb(meta_cat[:, i]) for i, emb in enumerate(self.meta_cat)],
-            dim=1,
-        )
-        meta = self.meta_hidden(meta)
-        macro = self.macro(macro)
-
-        x = torch.cat((sf, meta, macro), dim=1)
-        x = self.predict(x)
-
-        return x
-
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
@@ -179,23 +104,53 @@ class TemporalConvNet(nn.Module):
 
 
 class TcnV1(nn.Module):
-    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout):
+    def __init__(self, input_size, output_size, num_channels, kernel_size, dropout, meta_cont_lens, meta_cat_lens, hd, meta_hd, **params):
         super().__init__()
         self.tcn = TemporalConvNet(
             input_size, num_channels, kernel_size=kernel_size, dropout=dropout
         )
-        self.linear = nn.Linear(num_channels[-1], output_size)
-        self.init_weights()
+        self.meta_cont = nn.Sequential(
+            nn.Linear(meta_cont_lens[0], meta_cont_lens[1]),
+            nn.ReLU(),
+        )
+
+        self.meta_cat = nn.ModuleList(
+            [nn.Embedding(in_, out) for in_, out in meta_cat_lens]
+        )
+
+        self.meta_hidden = nn.Sequential(
+            nn.Linear(
+                meta_cont_lens[1] + sum(map(itemgetter(1), meta_cat_lens)), meta_hd
+            ),
+            nn.ReLU(),
+        )
+
+        self.predict = (
+            nn.Linear(num_channels[-1] + meta_hd, hd),
+            nn.ReLU(),
+            nn.Linear(hd, output_size),
+        )
+        # self.init_weights()
 
     def init_weights(self):
-        self.linear.weight.data.normal_(0, 0.01)
+        self.predict.weight.data.normal_(0, 0.01)
 
-    def forward(self, x):
-        y1 = self.tcn(x)
-        return self.linear(y1[:, :, -1])
+    def forward(self, x, cont, cat):
+        y = self.tcn(x)
+        
+        meta = self.meta_hidden(
+            torch.cat(
+                [self.meta_cont(cont)]
+                + [emb(cat[:, i]) for i, emb in enumerate(self.meta_cat)],
+                dim=1,
+            )
+        )
+
+        y = self.predict(torch.cat([y[:, :, -1], meta], dim=1))
+
+        return y
 
 
-models = dict(
-    NaiveMultivariateNetworkV1=NaiveMultivariateNetworkV1,
+tcn_models = dict(
     TcnV1=TcnV1,
 )
