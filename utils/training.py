e3236e96c4d5147e983ff47413bfdac36c7bc19a
ankile <lars.ankile@gmail.com>
2022-06-02 19:22:07 +0200

    Add MSE loss function

--- utils/training.py
+++ utils/training.py
@@ -63,6 +63,7 @@ def mape_loss(target: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
     ).mean()
     return l
 
+
 def mape_loss_2(target: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
     mask = (~target.isnan()) & (target.abs() >= 1e-2)
     denom = mask.sum(dim=1)
@@ -72,9 +73,19 @@ def mape_loss_2(target: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
     ).mean()
     return l
 
+
+def mse_loss_2(target: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
+    mask = (~target.isnan()) & (target.abs() >= 1e-2)
+    denom = mask.sum(dim=1)
+    target[target != target] = 0
+    l = ((((y_pred - target) ** 2 * mask)).sum(dim=1) / denom).mean()
+    return l
+
+
 loss_fns = dict(
     mape=mape_loss,
     mape_2=mape_loss_2,
+    mse_2=mse_loss_2,
 )
 
 
