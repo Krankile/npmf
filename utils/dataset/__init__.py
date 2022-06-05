5a1fd8f00179b5c61f3327fbffafd304873e7db6
Kjartan Krange <kjartankrange@outlook.com>
2022-06-05 17:30:04 +0200

    Add eracontroller to init

--- utils/dataset/__init__.py
+++ utils/dataset/__init__.py
@@ -1,8 +1,10 @@
 from .era_dataset import EraDataset
 from .timedelta_dataset import TimeDeltaDataset
+from .era_controller import EraController
 
 
 __all__ = [
     EraDataset,
     TimeDeltaDataset,
+    EraController,
 ]
