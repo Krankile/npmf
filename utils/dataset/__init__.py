04e4c37161fc888559e8e0febc847123bc7ea988
ankile <lars.ankile@gmail.com>
2022-06-07 15:52:41 +0200

    Remove TimedeltaDataset from init as well

--- utils/dataset/__init__.py
+++ utils/dataset/__init__.py
@@ -1,11 +1,9 @@
 from .era_controller import EraController
 from .era_dataset import EraDataset
-from .timedelta_dataset import TimeDeltaDataset
 
 
 
 __all__ = [
     EraDataset,
-    TimeDeltaDataset,
     EraController,
 ]
