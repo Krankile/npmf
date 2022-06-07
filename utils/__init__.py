5ccb58e3840b61a3fd2d4a3f4cec3072498106f0
ankile <lars.ankile@gmail.com>
2022-06-07 13:49:41 +0200

    Add problem definition enum class to utils

---
+++ utils/__init__.py
@@ -0,0 +1,32 @@
+class MCLoss:
+    mse = "mse_2"
+    mape = "mape_2"
+
+
+class VolLoss:
+    std_diff = "std_diff"
+
+
+class FundamentalLoss:
+    pass
+
+
+class MarketCap:
+    name = "market_cap"
+    loss = MCLoss
+
+
+class Volatility:
+    name = "volatility"
+    loss = VolLoss
+
+
+class Fundamentals:
+    name = "fundamentals"
+    loss = FundamentalLoss
+
+
+class Problem:
+    market_cap = MarketCap
+    volatity = Volatility
+    fundamentals = Fundamentals
