5ca2b1b3b0a27f0c3d45b9e6aa3807d8fc0686ee
ankile <lars.ankile@gmail.com>
2022-06-10 10:53:05 +0200

    Make naive prediction float not bool

--- utils/training.py
+++ utils/training.py
@@ -255,7 +255,7 @@ def get_naive_pred(data, target, device, conf):
 
     if conf.forecast_problem == Problem.fundamentals.name:
         if conf.train_loss == Problem.fundamentals.loss.ce_bankruptcy:
-            return three_balance_to_bankrupt(data[:, slice(9, 12), -conf.forecast_w:])
+            return three_balance_to_bankrupt(data[:, slice(9, 12), -conf.forecast_w:]).to(torch.float32)
 
         subset = conf.get("fundamental_targets") or slice(1, 18 + 1)
         return data[:, subset, -1]
